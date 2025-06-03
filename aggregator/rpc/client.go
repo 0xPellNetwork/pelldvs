58493a7f6b08daafb13b46d4de1578e4c848ba38
jimmy <jimmy@jimmzai.com>
2025-06-03 14:21:28 +0800

    improve aggregator client

--- aggregator/rpc/client.go
+++ aggregator/rpc/client.go
@@ -1,27 +1,37 @@
 package rpc
 
 import (
+	"errors"
 	"fmt"
 	"net/rpc"
 
+	"github.com/0xPellNetwork/pelldvs-libs/log"
 	aggTypes "github.com/0xPellNetwork/pelldvs/aggregator"
+	"github.com/0xPellNetwork/pelldvs/libs/clientmanager/rpcclientmanager"
+)
+
+const (
+	RPCHealthCheckMethod      = "RPCServerAggregator.HealthCheck"
+	RPCServerAggregatorMethod = "RPCServerAggregator.CollectResponseSignature"
 )
 
 // RPCClientAggregator provides a client implementation of the Aggregator interface
 // communicating with the aggregator service over RPC
 type RPCClientAggregator struct {
-	client *rpc.Client
+	clientManager *rpcclientmanager.RPCClientManager // Use RPCClientManager for connection management
+	logger        log.Logger
 }
 
 // NewRPCClientAggregator creates a new client instance connected to the specified address
 // establishing a connection to the aggregator RPC server
-func NewRPCClientAggregator(address string) (*RPCClientAggregator, error) {
-	client, err := rpc.Dial("tcp", address)
+func NewRPCClientAggregator(address string, logger log.Logger) (*RPCClientAggregator, error) {
+	manager, err := rpcclientmanager.NewRPCClientManager(address, RPCHealthCheckMethod, logger)
 	if err != nil {
 		return nil, fmt.Errorf("failed to connect to aggregator: %v", err)
 	}
 	return &RPCClientAggregator{
-		client: client,
+		clientManager: manager,
+		logger:        logger,
 	}, nil
 }
 
@@ -29,10 +39,39 @@ func NewRPCClientAggregator(address string) (*RPCClientAggregator, error) {
 // to the RPC server and receiving the validated response
 func (ra *RPCClientAggregator) CollectResponseSignature(response *aggTypes.ResponseWithSignature, validatedResponseCh chan<- aggTypes.ValidatedResponse) error {
 	var result aggTypes.ValidatedResponse
-	err := ra.client.Call("RPCServerAggregator.CollectResponseSignature", response, &result)
+	client, err := ra.clientManager.GetClient()
+	if err != nil {
+		return fmt.Errorf("failed to get RPC client: %v", err)
+	}
+	err = client.Call(RPCServerAggregatorMethod, response, &result)
+
+	if errors.Is(err, rpc.ErrShutdown) {
+		// If the client is shutdown, try to reconnect
+		ra.logger.Info("RPC client is shutdown, attempting to reconnect")
+		client, err = ra.clientManager.GetClient()
+		if err != nil {
+			return fmt.Errorf("failed to get RPC client after shutdown: %v", err)
+		}
+		err = client.Call(RPCServerAggregatorMethod, response, &result)
+	}
+
 	if err != nil {
 		return fmt.Errorf("failed to call aggregator RPC method: %v", err)
 	}
 	validatedResponseCh <- result
 	return nil
+}
+
+// HealthCheck performs a health check on the aggregator service
+func (ra *RPCClientAggregator) HealthCheck() (bool, error) {
+	var result bool
+	client, err := ra.clientManager.GetClient()
+	if err != nil {
+		return false, fmt.Errorf("failed to get RPC client: %v", err)
+	}
+	err = client.Call(RPCHealthCheckMethod, struct{}{}, &result)
+	if err != nil {
+		return false, fmt.Errorf("failed to call aggregator RPC method: %v", err)
+	}
+	return result, nil
 }
---
+++ libs/clientmanager/rpcclientmanager/manager_test.go
@@ -0,0 +1,137 @@
+package rpcclientmanager
+
+import (
+	"errors"
+	"fmt"
+	"net"
+	"net/rpc"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/huandu/go-assert"
+
+	"github.com/0xPellNetwork/pelldvs-libs/log"
+)
+
+type TestRPCServer struct {
+	rpcAddress string
+	isHealthy  bool
+	server     *rpc.Server
+	listener   net.Listener
+}
+
+func (trs *TestRPCServer) setIsHealthy() {
+	trs.isHealthy = true
+}
+
+func (trs *TestRPCServer) setUnHealthy() {
+	trs.isHealthy = false
+}
+
+func (ra *TestRPCServer) HealthCheck(_ struct{}, reply *bool) error {
+	*reply = ra.isHealthy
+	return nil
+}
+
+func (ra *TestRPCServer) Now(_ struct{}, reply *int) error {
+	*reply = time.Now().Nanosecond()
+	return nil
+}
+
+func newTestRPCServer() (*TestRPCServer, error) {
+	// Register the TestRPCServer
+	testServer := &TestRPCServer{
+		rpcAddress: "localhost:12366",
+		isHealthy:  true,
+		server:     rpc.NewServer(),
+	}
+	if err := testServer.server.Register(testServer); err != nil {
+		panic(err)
+	}
+	listener, err := net.Listen("tcp", testServer.rpcAddress)
+	if err != nil {
+		return nil, fmt.Errorf("unable to listen on address %s: %v", testServer.rpcAddress, err)
+	}
+
+	testServer.listener = listener
+	// Start the RPC server in a goroutine
+	go testServer.server.Accept(testServer.listener)
+
+	return testServer, err
+}
+
+func TestGetClient(t *testing.T) {
+	var logger = log.NewLogger(os.Stdout)
+	server, err := newTestRPCServer()
+	if err != nil {
+		t.Fatalf("failed to create test RPC server: %v", err)
+	}
+	defer func(listener net.Listener) {
+		err := listener.Close()
+		if err != nil {
+			t.Fatalf("failed to close listener: %v", err)
+		}
+	}(server.listener)
+
+	clientManager, err := NewRPCClientManager(server.rpcAddress, "TestRPCServer.HealthCheck", logger)
+	if err != nil {
+		t.Fatalf("failed to create RPC client manager: %v", err)
+	}
+
+	client, err := clientManager.GetClient()
+	if err != nil {
+		t.Fatalf("failed to get RPC client: %v", err)
+	}
+	var now int
+	err = client.Call("TestRPCServer.Now", struct{}{}, &now)
+	t.Log("now1", now)
+	assert.AssertEqual(t, err, nil)
+	assert.Assert(t, now > 0)
+
+	// Test health check
+	var isHealthy bool
+	err = client.Call("TestRPCServer.HealthCheck", struct{}{}, &isHealthy)
+	assert.AssertEqual(t, err, nil)
+	assert.AssertEqual(t, isHealthy, true)
+
+	server.setUnHealthy()
+	err = client.Call("TestRPCServer.HealthCheck", struct{}{}, &isHealthy)
+	assert.AssertEqual(t, err, nil)
+	assert.AssertEqual(t, isHealthy, false)
+
+	// test reconnection
+	// recreate testserver
+
+	client.Close()
+	server.setIsHealthy()
+
+	// set now to zero
+	now = 0
+	// this should fail because the server is restart
+	err = client.Call("TestRPCServer.Now", struct{}{}, &now)
+	t.Log("err===", err)
+	assert.AssertNotEqual(t, err, nil)
+	assert.AssertEqual(t, err, rpc.ErrShutdown)
+
+	time.Sleep(2 * time.Second) // wait for manager to reconnect
+
+	// test reconnection
+	client, err = clientManager.GetClient()
+	if err != nil {
+		t.Fatalf("failed to get RPC client after server restart: %v", err)
+	}
+	err = client.Call("TestRPCServer.Now", struct{}{}, &now)
+	if err != nil {
+		if errors.Is(err, rpc.ErrShutdown) {
+			client, err = clientManager.GetClient()
+			if err != nil {
+				t.Fatalf("failed to get RPC client after server restart: %v", err)
+			}
+		}
+	}
+	t.Log("err======", err)
+	t.Log("now2", now)
+	assert.AssertEqual(t, err, nil)
+	assert.Assert(t, now > 0)
+}
